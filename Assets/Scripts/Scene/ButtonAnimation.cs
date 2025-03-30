f89657f69b8ddf0cefa061be1ffe372f020fc5ca
Tarık Kıyılı <tarikkyli@gmail.com>
2025-03-30 12:38:37 +0300

    Panel Addings

--- Assets/Scripts/Scene/ButtonAnimation.cs
+++ Assets/Scripts/Scene/ButtonAnimation.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI; // Shadow için bu gerekli
 
 public class ButtonAnimation : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
@@ -10,11 +11,18 @@ public class ButtonAnimation : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     {
         rect = GetComponent<RectTransform>();
         originalScale = rect.localScale;
+
+        Shadow shadow1 = gameObject.AddComponent<Shadow>();
+        shadow1.effectColor = new Color(0, 0, 0, 0.3f);
+        shadow1.effectDistance = new Vector2(7, -7);
+        Shadow shadow2 = gameObject.AddComponent<Shadow>();
+        shadow2.effectColor = new Color(0, 0, 0, 0.1f);
+        shadow2.effectDistance = new Vector2(9, -9);
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        rect.localScale = originalScale * 0.95f; // biraz küçülerek "basılıyor" hissi
+        rect.localScale = originalScale * 0.95f;
     }
 
     public void OnPointerUp(PointerEventData eventData)
